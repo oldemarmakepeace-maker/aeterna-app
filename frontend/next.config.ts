4d3526722ca4777f1dfdd319b3bebc2ff527d985
oldemarmakepeace-maker <oldemarmakepeace@gmail.com>
2026-05-09 12:25:26 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
+  
 });
 
 const nextConfig: NextConfig = {
